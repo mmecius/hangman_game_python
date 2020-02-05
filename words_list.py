488360107719c804137da11c34230e19374256dd
Tomas-Mecius <tmecius@gmail.com>
2020-02-05 09:36:20 +0200

    Add random word choice from the words.txt

--- words_list.py
+++ words_list.py
@@ -1,2 +1,13 @@
-def hi_word():
-    print("hi words")
+import random
+
+word_list_url = ("words.txt")
+with open(word_list_url) as file_object:
+    try:
+        words_list = file_object.read().split()
+        print(words_list)
+    except:
+        print('File cannot be opened:', word_list_url)
+        quit()
+
+chosen_word = random.choice(words_list).lower()
+print(chosen_word)
